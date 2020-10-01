f78f0205b6351c11fbf40de24633b5a9ccf7f649
purriz <benjames8850@gmail.com>
2020-10-01 00:47:31 -0700

    war method in html

--- ninja_app/urls.py
+++ ninja_app/urls.py
@@ -7,13 +7,8 @@ urlpatterns = [
     path('logout', views.logout),
     path('game', views.index),
     path('get_rations', views.get_rations),
-    # path('train', views.train),
-    # path('process_money', views.process),
-    #remove this after testing
-    # path('clear_rations', views.clear),
     path('process', views.game),
     path('reset', views.reset),
-    #path('countdown', views.countdown),
     path('textgame', views.game),
     path('train', views.train),
     path('war', views.war)
