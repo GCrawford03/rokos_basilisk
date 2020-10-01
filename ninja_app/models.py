de3d721154f503047844ffa7fb6f9eea5e2abd25
purriz <benjames8850@gmail.com>
2020-10-01 00:36:19 -0700

    stamina used in text game

--- ninja_app/models.py
+++ ninja_app/models.py
@@ -1,21 +1,26 @@
 from django.db import models
 
+class UserManager(models.Manager):
 
-class PlayerManager(models.Manager):
-    def reg_val(self, postData):
-        errors = {}
-        if len(postData['username']) < 2:
-            errors['username'] = ("Username must be at least 2 Characters") 
-        if len(postData['password']) <8:
-            errors['password'] = 'Password must be at least 8 characters!'
-        if postData['password'] != postData['confirm_password']:
-            errors['confirm_password'] = 'Password must match!'
+    def validate(self, postData):
+        errors={}
+        
+        if len(postData['first_name'])< 1:
+            errors['first_name']="Please type in your name."
+
+        #if len(postData['user_input'])< 1:
+            #errors['user_input']="Please type in the indicated word in the prompt."
+
+        #NEED ERROR STATEMENT FOR ANTYHING OTHER THAN INDICATED WORD IN PROMPT
+        #USE IF/ELSE BASED ON PROMPT INDCIATED WORD
+        
+           
         return errors
 
-class Player(models.Model):
-    username = models.CharField(max_length=40)
-    password = models.CharField(max_length=100)
-    created_at = models.DateTimeField(auto_now=True)
-    updated_at = models.DateTimeField(auto_now_add=True)
-    objects = PlayerManager()  
+class User(models.Model):
+
+    first_name = models.CharField(max_length = 45)
+    #user_input = models.CharField(max_length = 45)
+
+    objects = UserManager()
 
--- ninja_app/urls.py
+++ ninja_app/urls.py
@@ -4,14 +4,13 @@ from . import views
 urlpatterns = [
     path('', views.homepage),
     path('register', views.register),
-    path('login', views.login),
     path('logout', views.logout),
     path('game', views.index),
     path('get_rations', views.get_rations),
     # path('train', views.train),
-    path('process_money', views.process),
+    # path('process_money', views.process),
     #remove this after testing
-    path('clear_rations', views.clear),
+    # path('clear_rations', views.clear),
     path('process', views.game),
     path('reset', views.reset),
     #path('countdown', views.countdown),
